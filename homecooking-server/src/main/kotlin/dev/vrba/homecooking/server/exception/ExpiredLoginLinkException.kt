0cc55bf9723cedfbeee34fff5a507c77098a5273
Jiří Vrba <jiri@vrba.dev>
2025-08-04 11:02:09 +0200

    Formatting

--- homecooking-server/src/main/kotlin/dev/vrba/homecooking/server/exception/ExpiredLoginLinkException.kt
+++ homecooking-server/src/main/kotlin/dev/vrba/homecooking/server/exception/ExpiredLoginLinkException.kt
@@ -1,11 +1,8 @@
 package dev.vrba.homecooking.server.exception
 
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.web.client.HttpStatusCodeException
 
-class ExpiredLoginLinkException : HttpStatusCodeException(
-    HttpStatus.UNPROCESSABLE_ENTITY,
-) {
+class ExpiredLoginLinkException : HttpStatusCodeException(HttpStatus.UNPROCESSABLE_ENTITY) {
     override val message: String = "The provided login link is expired"
 }
