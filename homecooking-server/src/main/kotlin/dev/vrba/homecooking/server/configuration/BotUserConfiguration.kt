d98242b2c2c4d91d4d9151cbd4a624051500c6c3
Jiří Vrba <jiri@vrba.dev>
2025-08-04 10:56:23 +0200

    Implement auth for the bot user

---
+++ homecooking-server/src/main/kotlin/dev/vrba/homecooking/server/configuration/BotUserConfiguration.kt
@@ -0,0 +1,9 @@
+package dev.vrba.homecooking.server.configuration
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "homecooking.auth.bot")
+data class BotUserConfiguration(
+    val username: String,
+    val password: String
+)
--- homecooking-server/src/main/kotlin/dev/vrba/homecooking/server/security/SecurityConfiguration.kt
+++ homecooking-server/src/main/kotlin/dev/vrba/homecooking/server/security/SecurityConfiguration.kt
@@ -1,16 +1,28 @@
 package dev.vrba.homecooking.server.security
 
+import dev.vrba.homecooking.server.configuration.BotUserConfiguration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
+@EnableConfigurationProperties(BotUserConfiguration::class)
 class SecurityConfiguration {
 
+    companion object {
+        const val BOT_ROLE = "BOT_ROLE"
+        const val USER_ROLE = "USER_ROLE"
+    }
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -20,6 +32,25 @@ class SecurityConfiguration {
             .formLogin { it.disable() }
             .httpBasic { it.realmName("Homecooking") }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests {
+                it.requestMatchers("/api/v1/bot/**").hasAuthority(BOT_ROLE)
+                it.requestMatchers("/api/v1/user/**").hasAuthority(USER_ROLE)
+                it.anyRequest().permitAll()
+            }
             .build()
     }
+
+    @Bean
+    fun userDetailsService(botUserConfiguration: BotUserConfiguration): UserDetailsService {
+        val password = passwordEncoder().encode(botUserConfiguration.password)
+        val authorities = AuthorityUtils.createAuthorityList(BOT_ROLE)
+
+        val botUser = User(botUserConfiguration.username, password, authorities)
+        val users = listOf(botUser)
+
+        return InMemoryUserDetailsManager(users)
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
 }
