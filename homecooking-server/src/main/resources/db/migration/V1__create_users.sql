822246c33ee9ed7bc8b11c191495a4ca7447abaa
Jiří Vrba <jiri@vrba.dev>
2025-08-04 09:29:25 +0200

    Add a migration for users

---
+++ homecooking-server/src/main/resources/db/migration/V1__create_users.sql
@@ -0,0 +1,11 @@
+create table users
+(
+    id         serial  not null,
+    username   varchar not null,
+    avatar_url varchar not null,
+    discord_id varchar not null unique,
+    token      varchar
+);
+
+create unique index on users(discord_id);
+create unique index on users(token);
